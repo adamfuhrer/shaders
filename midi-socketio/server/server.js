d51bc3f48d4d256fb2977228d8923bf1132943a4
Adam Fuhrer <adamfuhrer@gmail.com>
2023-11-14 19:54:09 -0500

    testing client 2 https

--- midi-socketio/server/server.js
+++ midi-socketio/server/server.js
@@ -1,15 +1,22 @@
-const http = require('http');
+const https = require('https'); // Use the 'https' module
 const socketIo = require('socket.io');
 const cors = require('cors');
+const fs = require('fs');
 
-const server = http.createServer((req, res) => {
+// Read the SSL certificate and private key
+const privateKey = fs.readFileSync('./key.pem', 'utf8');
+const certificate = fs.readFileSync('./cert.pem', 'utf8');
+
+const credentials = { key: privateKey, cert: certificate };
+
+const server = https.createServer(credentials, (req, res) => {
   res.end('Socket.IO server');
 });
 
 // Configure Socket.IO with CORS
 const io = socketIo(server, {
   cors: {
-    origin: "*",  // Allow all origins (you can specify specific origins if needed)
+    origin: "*",  // Allow all origins
     methods: ["GET", "POST"]
   }
 });
